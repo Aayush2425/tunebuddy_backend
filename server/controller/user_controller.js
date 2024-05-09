2c4aae3abfa61d64c90a52a008107fa7a722255b
Aayush2425 <122793682+Aayush2425@users.noreply.github.com>
2024-05-09 09:02:02 +0530

    Changed api

--- server/controller/user_controller.js
+++ server/controller/user_controller.js
@@ -1,9 +1,9 @@
 import { Account, Room } from "../model/schemas.js";
 export const getUser = async (req, res) => {
   const { id } = req.body;
-
+  console.log(id);
   const user = await Account.findById(id);
-
+  console.log(user);
   res.status(200).json(user);
 };
 
