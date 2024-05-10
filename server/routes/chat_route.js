cd3b106fa3394332fd795c24215c8cfcc7633a42
Aayush2425 <122793682+Aayush2425@users.noreply.github.com>
2024-05-10 06:20:58 +0530

    get chat api

--- server/routes/chat_route.js
+++ server/routes/chat_route.js
@@ -2,5 +2,5 @@ import express from "express";
 const router = express.Router();
 import { createRoom, getRoom } from "../controller/chat_controller.js";
 router.post("/chat", createRoom);
-router.get("/chat", getRoom);
+router.post("/chatDetail", getRoom);
 export default router;
