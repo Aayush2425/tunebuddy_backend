326790acf9297bcaeef950dae383d3d5709f0349
Aayush2425 <122793682+Aayush2425@users.noreply.github.com>
2024-06-05 07:05:13 +0530

    routes changed

--- server/controller/user_controller.js
+++ server/controller/user_controller.js
@@ -1,12 +1,18 @@
 import { Account, Room } from "../model/schemas.js";
 export const getUser = async (req, res) => {
-  const { id } = req.body;
-  console.log(id);
-  const user = await Account.findById(id);
-  console.log(user);
-  res.status(200).json(user);
+  const { id } = req.query; // Assuming user ID is passed as a query parameter
+  try {
+    const user = await Account.findById(id); // Adjust based on your schema and how you fetch the user
+    if (user) {
+      res.status(200).json(user);
+    } else {
+      res.status(404).json({ error: "User not found" });
+    }
+  } catch (error) {
+    console.error("Error fetching user:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 };
-
 export const getSearchUser = async (req, res) => {
   const user = await Account.find(
     {},
